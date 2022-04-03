9bae6ddaf16d65882b5028099355778eee7f9773
VentoAureo230 <anthony.meny35@gmail.com>
2022-04-03 18:10:09 +0200

    Ajout de fichier concernant la création d'entreprise en BDD, il reste la fonction central mais comme elle n'est pas encore fonctionnel je ne la GIT pas encore

--- formulaires.py
+++ formulaires.py
@@ -6,3 +6,13 @@ from wtforms.validators import DataRequired
 class ConnexionFormulaire(FlaskForm):
     Email = EmailField(validators=[DataRequired()])
     Password = PasswordField(validators=[DataRequired()])
+
+
+class FormulaireCreationEntreprise(FlaskForm):
+    Nom = StringField(validators=[DataRequired()])
+    Siret = IntegerField(validators=[DataRequired()])
+    Adresse = StringField(validators=[DataRequired()])
+    Code = IntegerField(validators=[DataRequired()])
+    Ville = StringField(validators=[DataRequired()])
+    Description = StringField
+    URL = StringField
--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from flask import Flask, session, redirect, request, url_for, render_template, flash
-from connexion import connexion
 import firebase_admin
-from firebase_admin import credentials, firestore
-from CONNECTdb import connect_BD
-
-connect_BD()
+from CreateMethod import createEntreprise
+from connexion import connexion
 
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
 
 
+# Page de connexion administrateur
 @app.route('/connexion', methods=['GET', 'POST'])
 def connexion_test():
     return connexion()
 
 
+# Page d'ajout d'une entreprise dans la BDD
+@app.route('/ajout-entreprise', methods=['GET', 'POST'])
+def creation_entreprise():
+    return createEntreprise()
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
