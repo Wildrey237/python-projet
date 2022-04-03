c577ae87a94cf7dbc4d1a0924f6026c5562f136c
VentoAureo230 <anthony.meny35@gmail.com>
2022-04-03 16:53:22 +0200

    Clef de connexion enlerver le .sample a la fin

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from flask import Flask, session, redirect, request, url_for, render_template, f
 from connexion import connexion
 import firebase_admin
 from firebase_admin import credentials, firestore
-
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+from CONNECTdb import connect_BD
 
 
 app = Flask(__name__, template_folder='templates')
