307098dd9018e5b37c48b68e01116873a77ba858
Wildrey237 <wilfriedbemelingue@gmail.com>
2022-04-05 10:05:02 +0200

    imprimer pdf

--- MakePDF.py
+++ MakePDF.py
@@ -3,18 +3,17 @@ from datetime import datetime
 
 
 class PDFGenerator:
-    template = "google.com"
 
     def __init__(self, nom_facture: str = None):
         self.id_facture = nom_facture
 
-    def Make_PDF(self):
+    def Make_PDF(self, template):
         date = datetime.now()
-        id = str(date.strftime("%Y-%m-%d / %H:%M:%S"))
+        id = str(date.strftime("%Y-%m-%d %H:%M:%S"))
         name = f"facture n°{id}"
-        pdf = pdfkit.from_url(PDFGenerator.template, f'Facture/{name}.pdf')
+        pdf = pdfkit.from_url(template, f'Facture/{name}.pdf')
         return pdf
 
 
 fact = PDFGenerator()
-fact.Make_PDF()
+fact.Make_PDF('https://www.reddit.com/')
