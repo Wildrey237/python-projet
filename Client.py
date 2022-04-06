0f926bf3fa159800e1bcfa776028e92f03167bc9
VentoAureo230 <anthony.meny35@gmail.com>
2022-04-07 00:26:08 +0200

    HTML CSS

--- Client.py
+++ Client.py
@@ -1,8 +1,9 @@
-from Connexion import db
 from flask import redirect, url_for, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, IntegerField, TextAreaField, BooleanField, SubmitField
+from wtforms import StringField, EmailField, IntegerField, BooleanField
 from wtforms.validators import DataRequired
+
+from Connexion import db
 from Formulaires import FormulaireModificationClient
 
 
